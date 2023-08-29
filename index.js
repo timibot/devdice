2b5d874226d94aa05d1819d495dacad60e97bf49
TiMi <66627735+timibot@users.noreply.github.com>
2023-08-29 16:35:13 +0100

    Update index.js
    make the naming more specific for player 2 to aid understanding

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ image1.setAttribute("src", randomImageSource);
 
 //Player 2
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var randomDiceImage = "dice" + randomNumber2 + ".png";
-var randomImageSource = "images/" + randomDiceImage;
-var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomImageSource);
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+var randomImageSource2 = "images/" + randomDiceImage2;
+var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 
 //if player 1 wins
